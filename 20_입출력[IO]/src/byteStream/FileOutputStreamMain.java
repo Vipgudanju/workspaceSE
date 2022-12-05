d8a2c6027c9667627ddcad2674f315a9e745b1a2
VipGudanju <jsaprk33@naver.com>
2022-12-05 11:23:10 +0900

    ..

--- 20_입출력[IO]/src/byteStream/FileOutputStreamMain.java
+++ 20_입출력[IO]/src/byteStream/FileOutputStreamMain.java
@@ -26,6 +26,7 @@ public class FileOutputStreamMain {
 		fos.write(66);
 		fos.write(0b00000000000000000000000001000011);
 		fos.write(0b01111111111111111111111111111111);
+		fos.write(0b01111111111111111111111111111111);
 		
 		for(int i = 0; i < 256; i++) {
 			fos.write(i);
