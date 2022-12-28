63d44bb91bb717762a333eabdd773554df66b8c2
VipGudanju <jsaprk33@naver.com>
2022-12-28 18:55:34 +0900

    ..

--- 25.JDBC[Java DataBase Connectvity]/src/dao/guest/GuestDao.java
+++ 25.JDBC[Java DataBase Connectvity]/src/dao/guest/GuestDao.java
@@ -1,5 +1,9 @@
 package dao.guest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /*
  * Dao(Data[DataBase] Access Object)객체(클래스)
  *   - guest테이블에 CRUD(Create,Read,Update,Delet)작업을 하는 
@@ -7,12 +11,38 @@ package dao.guest;
  */
 public class GuestDao {
 	
-	public void insert() throws Exception {
+	public void insert(Guest newGuest) throws Exception {
+		String driverClass = "oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@localhost:1521:xe";
+		String user="scott";
+		String password="tiger";
+		
+		String insertSQL = "insert into Guest(guest_no, guest_name, guest_date, guest_email, guest_homepage, guest_title, guest_content) values"
+											 + "(" +newGuest.getGuest_no() +"',"
+											 + "'" + newGuest.getGuest_name() +"',"
+											 + "'" + newGuest.getGuest_date() +"',"
+											 + "'" + newGuest.getGuest_email() +"',"
+											 + "'" + newGuest.getGuest_homepage() +"',"
+											 + "'" + newGuest.getGuest_title() +"',"
+											 + "'" + newGuest.getGuest_content()+"')";
+		Class.forName(driverClass);
+		Connection con = DriverManager.getConnection(url, user, password);
+		Statement stmt = con.createStatement();
+		int rowCount = stmt.executeUpdate(insertSQL);
+		stmt.close();
+		con.close();
 		
 	}
 	
 	
-	public void update() throws Exception {
+	public void update(Guest newGuest) throws Exception {
+		
+		String driverClass = "oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@localhost:1521:xe";
+		String user="scott";
+		String password="tiger";
+		
+		
 		
 	}
 	
