89c3bae1c98821fb308f55272dd7ffe2ef1537b4
VipGudanju <jsaprk33@naver.com>
2023-01-09 11:14:42 +0900

    ..

--- java-project-class5-shop/src/com/itwill/shop/ui/박준식/ProductListPanel_박준식.java
+++ java-project-class5-shop/src/com/itwill/shop/ui/박준식/ProductListPanel_박준식.java
@@ -2,6 +2,7 @@ package com.itwill.shop.ui.박준식;
 
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JButton;
 
 public class ProductListPanel_박준식 extends JPanel {
 	/*
@@ -17,6 +18,11 @@ public class ProductListPanel_박준식 extends JPanel {
 	 */
 	public ProductListPanel_박준식() {
 		setBackground(new Color(128, 128, 255));
+		setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(241, 159, 97, 23);
+		add(btnNewButton);
 
 	}
 
